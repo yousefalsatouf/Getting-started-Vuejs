b2946d680df83644c2f698c6355a20955e77537f
[Yousef Alsatouf] <yousef.alsatouf94@gmail.com>
2019-09-25 11:02:30 +0200

    getting started with vuejs library

---
+++ js/main.js
@@ -0,0 +1,38 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello Vue From Yousef!'
+    }
+});
+var app2 = new Vue({
+    el: "#app-2",
+    data: {
+        message: "you loaded this page on " + new Date().toLocaleString()
+    }
+});
+var app3 = new Vue({
+    el: "#app-3",
+    data: {
+        seen: true
+    }
+});
+var app4 = new Vue({
+    el: '#app-4',
+    data: {
+        todos: [{
+                text: 'Learn JavaScript'
+            },
+            {
+                text: 'Learn Vue'
+            },
+            {
+                text: 'Build something awesome'
+            }
+        ]
+    }
+});
+app4.todos.push({
+    text: 'learn React'
+});
+
+console.log(app4.todos);
